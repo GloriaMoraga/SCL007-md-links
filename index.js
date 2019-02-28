3c07e345998aae26dc6c183e273d27606e3bbe37
GloriaMoraga <silmeriel@gail.com>
2019-02-28 17:23:56 -0300

    muestra links con condicion --validate

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const fetch = require('node-fetch');
 const path = require('path');
 const colors = require('colors');
 const fileUser = process.argv[2];
+let options = {
+  validate: false,
+  stats: false
+}
 
 
 //------------------------ Funcion que retorna los links
 
-  const getDataMd = (fileName) => new Promise((resolve, err) => {
+  const mdLinks = (fileName) => new Promise((resolve, err) => {
           
             let urlToAbsolute = path.resolve(fileName);
             let extFile = path.extname(urlToAbsolute);
@@ -23,34 +27,16 @@ const fileUser = process.argv[2];
                 let dataStrings = dataLinks.toString().match(expRegLinks);
 
                 for (let i in dataStrings){
-                  let textLinkMarkdown = dataStrings[i].match(expRegLinktext)[0].substring(1, dataStrings[i].match(expRegLinktext)[0].length - 1);
-                  let urlLinkMarkdown = dataStrings[i].match(expRegHref)[0].match(expRegHref)[0].substring(1, dataStrings[i].match(expRegHref)[0].length - 1);
+                  let txtLinkMd = dataStrings[i].match(expRegLinktext)[0].substring(1, dataStrings[i].match(expRegLinktext)[0].length - 1);
+                  let urlLinkMd = dataStrings[i].match(expRegHref)[0].match(expRegHref)[0].substring(1, dataStrings[i].match(expRegHref)[0].length - 1);
                   //Este codigo permite agregar al array un objetos con las siguientes propiedades
                   linksMd.push({
-                    text: textLinkMarkdown,
-                    href: urlLinkMarkdown,
+                    text: txtLinkMd,
+                    href: urlLinkMd,
                     file: fileUser
                   });
                 }
                resolve(linksMd);
-               let arrStatus = [];
-    
-    Promise.all(linksMd.map(function(element){ 
-      fetch(element.href).then(res =>{
-        arrStatus.push(element);
-        if(res.status === 200){
-          console.log(`Archivo: ${element.file.blue} Texto: ${element.text.green} Link: ${res.url.yellow} Status: ${res.status} ${res.statusText.cyan}`);
-        }
-         else if(res.status === 404){
-          console.error(`Enlace roto --> Archivo: ${element.file.blue} Texto: ${element.text.green} Link: ${element.href.yellow}` + err);
-
-          } 
-      }).catch(err =>{
-        console.error(`Enlace roto --> Archivo: ${element.file.blue} Texto: ${element.text.red} Link: ${element.href.red}` + err);
-         
-      });
-    }));
-  
               //  console.log(linksMd)
               } else {
                 console.log('Archivo ingresado no es de extención .md');
@@ -59,7 +45,41 @@ const fileUser = process.argv[2];
                });
 
    
-  getDataMd(fileUser);
 
+mdLinks(fileUser, options).then( (response) =>{
+  let arrStatus = [];
+  if(process.argv[3] === '--validate'){
+     return response.forEach((element)=> {
+      fetch(element.href).then(res =>{
+        arrStatus.push(element);
 
+        if(res.status === 200){
+      
+          console.log(`File: ${element.file.blue} Titulo: ${element.text.yellow} Link: ${res.url.green} Status: ${res.status} ${res.statusText.cyan}`);
+        }
+         else if(res.status === 404){
+          console.log(`Enlace roto --> Archivo: ${element.file.blue} Texto: ${element.text.green} Link: ${element.href.yellow}` + err );
+
+          } 
+      }).catch(err =>{
+        console.log(`Enlace con Problemas --> Archivo: ${element.file.red} Texto: ${element.text.green} Link: ${element.href.red}` + err);
+         
+      });
+      
+    });
+
+  } else {
+
+    return response.forEach((element)=> {
+      let file = element.file,
+          href = element.href,
+          text = element.text;      
+      return console.log(file.blue  + ' ' + text.yellow + ' ' + href.green);
+    });
+    
+  }
+  
+});
+
+module.exports = mdLinks;
 
--- mdlinks.js
+++ mdlinks.js
@@ -1,3 +1,98 @@
+// #!/usr/bin/env node
+// const fs = require('fs');
+// const fetch = require('node-fetch');
+// const path = require('path');
+// const colors = require('colors');
+
+
+
+// //------------------------ Funcion que retorna los links
+
+//   const mdLinks = (fileName) => new Promise((resolve, err) => {
+          
+//             let urlToAbsolute = path.resolve(fileName);
+//             let extFile = path.extname(urlToAbsolute);
+//             if(extFile === '.md'){
+//             let dataMd = fs.readFileSync(urlToAbsolute).toString();
+//             let linksMd = [];
+//                const expRegLinks = /\[((.+?))\]\((http|https|ftp|ftps).+?\)/g;
+//                 const expRegHref =/\((http|https|ftp|ftps).+?\)/g;
+//                 const expRegLinktext = /\[.+?\]/g;
+//                 let dataLinks = dataMd.toString();
+//                 let dataStrings = dataLinks.toString().match(expRegLinks);
+
+//                 for (let i in dataStrings){
+//                   let txtLinkMd = dataStrings[i].match(expRegLinktext)[0].substring(1, dataStrings[i].match(expRegLinktext)[0].length - 1);
+//                   let urlLinkMd = dataStrings[i].match(expRegHref)[0].match(expRegHref)[0].substring(1, dataStrings[i].match(expRegHref)[0].length - 1);
+//                   //Este codigo permite agregar al array un objetos con las siguientes propiedades
+//                   linksMd.push({
+//                     text: txtLinkMd,
+//                     href: urlLinkMd,
+//                     file: fileUser
+//                   });
+//                 }
+//                resolve(linksMd);
+//                let arrStatus = [];
+    
+//     Promise.all(linksMd.map(function(element){ 
+//       fetch(element.href).then(res =>{
+//         arrStatus.push(element);
+
+//         if(res.status === 200){
+//           console.log(`Archivo: ${element.file.blue} Texto: ${element.text.green} Link: ${res.url.yellow} Status: ${res.status} ${res.statusText.cyan}`);
+//         }
+//          else if(res.status === 404){
+//           console.error(`Enlace roto --> Archivo: ${element.file.blue} Texto: ${element.text.green} Link: ${element.href.yellow}` + err);
+
+//           } 
+//       }).catch(err =>{
+//         console.error(`Enlace roto --> Archivo: ${element.file.blue} Texto: ${element.text.red} Link: ${element.href.red}` + err);
+         
+//       });
+//     }));
+  
+//               //  console.log(linksMd)
+//               } else {
+//                 console.log('Archivo ingresado no es de extención .md');
+//               }
+          
+//                });
+
+   
+// mdLinks(fileUser);
+
+// module.exports = mdLinks;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const fs = require('fs');
 // const path = require('path');
