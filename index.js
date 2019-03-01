3cd2d3cc3c781762f6891daf374877dbce5c8958
GloriaMoraga <silmeriel@gail.com>
2019-03-01 17:10:56 -0300

    archivos js separados

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 const mdLinks = require('./mdlinks')
+const fetch = require('node-fetch');
+const colors = require('colors');
+const fileUser = process.argv[2];
+let options = {
+  validate: false,
+  stats: false
+}   
+if (require.main === module) {
+  mdLinks(fileUser, options).then( (response) =>{
+  let arrValidate = [];
+  
+  if(process.argv[3] === '--validate'){
+    options.validate = true;
+     return response.forEach((element)=> {
+      fetch(element.href).then(res =>{
+        arrValidate.push(element);
 
-  mdLinks()
+        if(res.status === 200){
+      
+          return console.log(`File: ${element.file.blue} Titulo: ${element.text.yellow} Link: ${res.url.green} Status: ${res.status} ${res.statusText.cyan}`);
+        }
+         else if(res.status === 404){
+          return console.log(`File: ${element.file.blue} Texto: ${element.text.yellow} Link: ${element.href.red} Status: ${res.status} ${res.statusText.red}` );
+
+          } 
+      }).catch(err =>{
+        console.log(`File: ${element.file.red} Texto: ${element.text.green} Link: ${element.href.red} <--- Enlace con Problemas` + err);
+         
+      });
+      
+    });
+
+  } else if(process.argv[3] === '--stats'){
+       options.stats = true;
+       let urlArray = [];
+        response.forEach(element => {
+          urlArray.push(element.href)
+          })
+         console.log('El total de links en el archivo ' + fileUser.cyan +' es: '+ + urlArray.length); 
+  }else {
+          return response.forEach((element)=> {
+          let file = element.file,
+          href = element.href,
+          text = element.text;      
+          return console.log(file.blue  + ' ' + text.yellow + ' ' + href.green);
+    });
+
+    
+  }
+  
+});
+}
--- mdlinks.js
+++ mdlinks.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
 const fetch = require('node-fetch');
 const path = require('path');
 const colors = require('colors');
 const fileUser = process.argv[2];
-let options = {
-  validate: false,
-  stats: false
-}
+
 
 //------------------------ Funcion que retorna los links
 const mdLinks = (fileName) => new Promise((resolve, err) => {
@@ -39,53 +35,6 @@ const mdLinks = (fileName) => new Promise((resolve, err) => {
               } else {console.log('Archivo ingresado no es de extención .md');}
 });
 
-   
-if (require.main === module) {
-  mdLinks(fileUser, options).then( (response) =>{
-  let arrValidate = [];
-  
-  if(process.argv[3] === '--validate'){
-    options.validate = true;
-     return response.forEach((element)=> {
-      fetch(element.href).then(res =>{
-        arrValidate.push(element);
 
-        if(res.status === 200){
-      
-          return console.log(`File: ${element.file.blue} Titulo: ${element.text.yellow} Link: ${res.url.green} Status: ${res.status} ${res.statusText.cyan}`);
-        }
-         else if(res.status === 404){
-          return console.log(`File: ${element.file.blue} Texto: ${element.text.yellow} Link: ${element.href.red} Status: ${res.status} ${res.statusText.red}` );
-
-          } 
-      }).catch(err =>{
-        console.log(`File: ${element.file.red} Texto: ${element.text.green} Link: ${element.href.red} <--- Enlace con Problemas` + err);
-         
-      });
-      
-    });
-
-  } else if(process.argv[3] === '--stats'){
-       options.stats = true;
-       let urlArr = [];
-        response.map(element => {
-          urlArr.push(element.href)
-        
-        return console.log('Total : '+ urlArr.length);
-         })
-         
-  }else {
-          return response.forEach((element)=> {
-          let file = element.file,
-          href = element.href,
-          text = element.text;      
-          return console.log(file.blue  + ' ' + text.yellow + ' ' + href.green);
-    });
-
-    
-  }
-  
-});
-}
 module.exports = mdLinks;
 
