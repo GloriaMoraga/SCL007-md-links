f62f55b773dc513a6bf9815befd518f3f7773d12
GloriaMoraga <silmeriel@gail.com>
2019-03-04 01:21:07 -0300

    actualizando Readme

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,20 @@
-const mdLinks = require('../');
+const mdLinks = require('../mdlinks.js');
+const chai = require('chai');
+const index = require('../index.js')
 
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('Debería ser una función', () => {
+    expect(typeof mdLinks).toBe('function');
+  });
+});
+
+
+describe('index', () => {
+
+  it('Debería ser un objeto', () => {
+    expect(typeof index).toBe('function');
   });
 
 });
